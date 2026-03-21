adefbf54016d4d5ba10944df27232dc85e1c7b43
RoseGoldJaguar <kd00286@my.westga.edu>
2026-03-21 18:10:37 -0400

    Attempt to submit

---
+++ Desktop/Assign2.sql
@@ -0,0 +1,7 @@
+INSERT INTO guests (guest_id, name, phone_number, email)
+Values (12833, 'Nancy Brown', 8635823, 'nancybrown@outlook.com');
+
+INSERT INTO guests (guest_id, name, phone_number, email)
+Values (11823, 'Velma Simpson', 8635823, 'velmasimpson@outlook.com');
+
+					
